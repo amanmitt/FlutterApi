80ff6f94b10281bc003e3ca99d340d3c13368227
Aman <aman280@outlook.com>
2021-01-13 17:23:23 +0530

    branch1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter New Branch',
+      title: 'Back to Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
